64e919c0c46b3e93aa7017a86c02ce389c3f9f9a
APanRU <eklmn2004@mail.ru>
2022-10-05 23:13:20 +0300

    add_Lecture_1

---
+++ Lecture/Lecture_1/lecture_1.py
@@ -0,0 +1,119 @@
+import collections
+import numbers
+from operator import truediv
+import re
+from sys import orig_argv
+from turtle import right
+from unittest import TestCase
+
+
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(b)
+# s = "hello world"
+# print(s)
+# print(a, b, s)
+# print('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+# f = True
+# print(f)
+
+# list = ['1', '2', '3']
+# print(list)
+
+# print("Введите a: ")
+# a = int(input())
+# print("Введите b: ")
+# b = int(input())
+# print(a + b)
+
+# a = 123
+# b = -321
+# c = a+b
+# print(c)
+
+# a = 1.23
+# b = 3
+# c = round (a*b,3)
+# print(c)
+
+# a = 3
+# a = a + 5
+# a += 5
+# print(a)
+
+# a = 1 < 3 and 3 < 5
+# print(a)
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура! Это Маша!')
+# elif username == 'Марина':
+#     print('Я так ждал Вас, Марина!')
+# else:
+#     print('Привет, ', username)
+
+# original = 23
+# inverted = 0
+# while original !=0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+# else:
+#     print('end')
+# print(inverted)
+
+# for i in 1,2,3,4,5:
+#     print(i**2)
+
+# for i in range(1, 10, 2):
+#     print(i)
+
+# text = 'съешь еще этих мягких французских булок'
+# print(len(text))
+# print('еще' in  text)
+# print(text.isdigit())
+# print(text.islower())
+# print(text.replace('еще', 'ЕЩЕ'))
+
+# numbers = [1, 2, 3, 4, 5]
+# print(numbers)
+# ran = range(10)
+# numbers = list(ran)
+# print(numbers)
+# numbers[0] = 10
+# print(numbers)
+# for i in numbers:
+#     i *= 2
+#     print(i)
+# print(numbers)
+
+# colors = ['red', 'green', 'blue']
+# for e in colors:
+#     print(e*2)
+
+# colors.append('gray')
+# print(colors)
+# colors.remove('red')
+# print(colors)
+
+# def f(x):
+#     if x == 1:
+#         return "Целое"
+#     elif x == 2.3:
+#         return 23
+#     else:
+#         return
+
+# arg = 1
+# print(f(arg))
+# print(type(f(arg)))
+
+
