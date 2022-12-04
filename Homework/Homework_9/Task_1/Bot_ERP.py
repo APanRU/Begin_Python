36560719080426672c1f653e49ddfa42af48f181
APanRU <eklmn2004@mail.ru>
2022-12-04 14:28:57 +0300

    add_Homework_9

---
+++ Homework/Homework_9/Task_1/Bot_ERP.py
@@ -0,0 +1,87 @@
+import telebot
+import json
+import random
+
+API_TOKEN='5980688680:AAEBil6j3zHWB7uwOkMQS3DJuZ6oXaYsw2o'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+calc = False
+data = {}
+def uniqueid():
+    seed = random.getrandbits(32)
+    while True:
+       yield seed
+       seed += 1
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    global data
+    with open(r'Begin_Python\Homework\Homework_9\Task_1\data.json', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/Команды', '/Справочник')
+        bot.send_message(message.chat.id, 'Привет! Что будем делать с телефонным справочником?', reply_markup=keyboard)
+    
+@bot.message_handler(commands=['Команды'])
+def comands_message(message):
+    bot.send_message(message.chat.id,'Чтобы добавить новый контакт, введите команду: \n \
+        /Добавить, Ф.И.О. через пробел и номер телефона \n \
+            (/Добавить Иванов Иван Иванович 89601234567)')
+    bot.send_message(message.chat.id,'Чтобы удалить контакт, введите команду: \n \
+        /Удалить и через ID контакта  \n \
+            (/Удалить 3474373417)')
+    bot.send_message(message.chat.id,'Чтобы найти контакт, введите команду: \n \
+        /Найти и ID контакта через пробел \n \
+            (/Найти 3474373417)')
+        
+@bot.message_handler(commands=['Справочник'])
+def comands_message(message):
+    global data
+    with open(r'Begin_Python\Homework\Homework_9\Task_1\data.json', 'rb') as json_file:
+        data = json_file.read()
+    bot.send_message(message.chat.id, data)
+
+@bot.message_handler(content_types='text')
+def check_message(message):
+    global calc
+    if '/Добавить' in message.text:
+        bot.send_message(message.chat.id,'Добавили!')
+        msg = message.text
+        msg = msg.split(' ')
+        name = [msg[1], msg[2], msg[3]]
+        name = ' '.join(name)
+        unique_sequence = uniqueid()
+        ID = next(unique_sequence)
+        with open(r'Begin_Python\Homework\Homework_9\Task_1\data.json', encoding='utf-8') as json_file:
+            data = {}
+            data = json.load(json_file)
+        data[ID] = {
+            'Name': name,
+            'Phone': msg[4]
+        }
+        with open(r'Begin_Python\Homework\Homework_9\Task_1\data.json', 'w', encoding='utf-8') as outfile:
+            json.dump(data, outfile, indent=2, ensure_ascii=False)
+    elif '/Удалить' in message.text:
+        bot.send_message(message.chat.id,'Удалили!')
+        msg = message.text
+        msg = msg.split(' ')
+        with open(r'Begin_Python\Homework\Homework_9\Task_1\data.json', encoding='utf-8') as json_file:
+            data = {}
+            data = json.load(json_file) 
+        data.pop(msg[1], None)
+        with open(r'Begin_Python\Homework\Homework_9\Task_1\data.json', 'w', encoding='utf-8') as outfile:
+            json.dump(data, outfile, indent=2, ensure_ascii=False)
+    elif '/Найти' in message.text:
+        bot.send_message(message.chat.id,'Результат поиска:')
+        msg = message.text
+        msg = msg.split(' ')
+        msg = msg[1]
+        print(msg)
+        with open(r'Begin_Python\Homework\Homework_9\Task_1\data.json', encoding='utf-8') as json_file:
+            data = {}
+            data = json.load(json_file)
+            search = data[msg]
+        bot.send_message(message.chat.id, str(search))
+
+bot.polling()
---
+++ Homework/Homework_9/Task_1/README.md
@@ -0,0 +1,36 @@
+# Результат:
+ТГ бот умеет урезанный функционал по работе с телефонным справочником.
+Автор - Евгений Мамаев, Панкратов Андрей
+
+## Функции:
+1. Просмотреть весь справочник (кнопка)
+2. Вывести список доступных команд (кнопка)
+3. Добавить контакт (ФИО и телефон)
+4. Удалить контакт
+5. Найти контакт по ID
+
+
+![Start_messege](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Start_messege.png)
+Старт работы бота"
+Старт работы бота
+---
+![All](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/All.png)
+Вывод телефонного справочника"
+Вывод телефонного справочника
+---
+![Comands](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Comands.png)
+Вывод списка доступных команд"
+Вывод списка доступных команд
+---
+![Add_people](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Add_people.png)
+Добавление контакта"
+Добавление контакта
+---
+![Search_people](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Add_people.png)
+Поиск контакта"
+Поиск контакта
+---
+![Dell_people](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Dell_people.png)
+Удаление контакта"
+Удаление контакта
+---
